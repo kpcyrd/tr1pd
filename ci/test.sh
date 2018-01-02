534902aa982040190c65a0cea7b6f8961d209472
kpcyrd <git@rxv.cc>
2018-01-02 18:45:21 +0100

    travis: enable cargo color

--- ci/test.sh
+++ ci/test.sh
@@ -4,6 +4,6 @@ set -ex
 case "$BUILD_MODE" in
     *)
         docker build --build-arg TARGET="$TARGET" -t "tr1pd-test-$TARGET" -f ci/Dockerfile .
-        docker run -e TARGET="$TARGET" "tr1pd-test-$TARGET" ci/build.sh
+        docker run -t -e TARGET="$TARGET" "tr1pd-test-$TARGET" ci/build.sh
         ;;
 esac
