5c24e469f74d038c0015ee9b71f1f340a576b769
kpcyrd <git@rxv.cc>
2018-01-15 01:39:20 +0100

    seccomp aarch64 corrections

--- src/sandbox/syscalls.rs
+++ src/sandbox/syscalls.rs
@@ -25,6 +25,7 @@ pub enum Syscall {
     sched_getaffinity   = libc::SYS_sched_getaffinity   as isize,
     exit_group          = libc::SYS_exit_group          as isize,
     set_robust_list     = libc::SYS_set_robust_list     as isize,
+    #[cfg(not(target_arch = "aarch64"))]
     open                = libc::SYS_open                as isize,
     openat              = libc::SYS_openat              as isize,
     getrandom           = libc::SYS_getrandom           as isize,
@@ -45,6 +46,7 @@ pub enum Syscall {
     epoll_create1       = libc::SYS_epoll_create1       as isize,
     epoll_ctl           = libc::SYS_epoll_ctl           as isize,
     epoll_pwait         = libc::SYS_epoll_pwait         as isize,
+    #[cfg(not(target_arch = "aarch64"))]
     epoll_wait          = libc::SYS_epoll_wait          as isize,
     listen              = libc::SYS_listen              as isize,
     #[cfg(not(target_arch = "aarch64"))]
