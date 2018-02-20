90a94d98f8898e784152afa4c16cb0c5382e2a38
kpcyrd <git@rxv.cc>
2018-02-20 02:35:43 +0100

    Improve CI
    - aarch64
    - i686
    - reprotest
    - docker

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,8 +1,42 @@
 #!/bin/sh
 set -ex
 
+case "$TARGET" in
+    aarch64-unknown-linux-gnu)
+        dpkg --add-architecture arm64
+        ;;
+
+    i686-unknown-linux-gnu)
+        dpkg --add-architecture i386
+        ;;
+esac
+
 rustup install "stable-$TARGET"
 rustup target add "$TARGET" || true
 
 apt-get update -q
-apt-get install -qy libsodium-dev libseccomp-dev libzmq3-dev
+
+case "$TARGET" in
+    x86_64-unknown-linux-gnu)
+        apt-get install -qy \
+            libsodium-dev \
+            libseccomp-dev \
+            libzmq3-dev
+        ;;
+    aarch64-unknown-linux-gnu)
+        apt-get install -qy gcc-6-aarch64-linux-gnu \
+            libsodium-dev:arm64 \
+            libseccomp-dev:arm64 \
+            libzmq3-dev:arm64
+        ;;
+    i686-unknown-linux-gnu)
+        apt-get install -qy gcc-multilib \
+            libsodium-dev:i386 \
+            libseccomp-dev:i386 \
+            libzmq3-dev:i386
+        ;;
+    *)
+        echo "UNKNOWN TARGET: $TARGET"
+        exit 1
+        ;;
+esac
--- ci/test.sh
+++ ci/test.sh
@@ -2,6 +2,14 @@
 set -ex
 
 case "$BUILD_MODE" in
+    docker)
+        docker build -t tr1pd .
+        docker images tr1pd
+        ;;
+    reprotest)
+        docker build -t reprotest-tr1pd -f ci/Dockerfile.reprotest .
+        docker run --privileged reprotest-tr1pd ci/reprotest.sh
+        ;;
     *)
         docker build --build-arg TARGET="$TARGET" -t "tr1pd-test-$TARGET" -f ci/Dockerfile .
         docker run -t -e TARGET="$TARGET" "tr1pd-test-$TARGET" ci/build.sh
