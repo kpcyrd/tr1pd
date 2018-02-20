037de3b56a56a36976501e467fb8eb95da85749b
kpcyrd <git@rxv.cc>
2018-02-20 03:03:21 +0100

    Fix cross-compilation

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 set -ex
-cargo build --verbose --all
-cargo test --verbose --all
+
+case "$TARGET" in
+    x86_64-unknown-linux-gnu)
+        cargo build --verbose --all
+        cargo test --verbose --all
+        ;;
+    aarch64-unknown-linux-gnu)
+        export RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc-6"
+        cargo build --verbose --all --target="$TARGET"
+        ;;
+    i686-unknown-linux-gnu)
+        cargo build --verbose --all --target="$TARGET"
+        ;;
+esac
