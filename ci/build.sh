fa2d948d4205fd729766d5e405919ccefa8af27d
kpcyrd <git@rxv.cc>
2018-03-05 02:40:11 +0100

    Disable crosscompile

--- ci/build.sh
+++ ci/build.sh
@@ -7,11 +7,14 @@ case "$TARGET" in
         cargo test --verbose --all
         ;;
     aarch64-unknown-linux-gnu)
-        export RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc-6"
+        export RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc-6 -C ar=aarch64-linux-gnu-gcc-ar-6"
+        export CC="aarch64-linux-gnu-gcc-6"
+        export PKG_CONFIG="aarch64-linux-gnu-pkg-config"
         export PKG_CONFIG_ALLOW_CROSS=1
         cargo build --verbose --all --target="$TARGET"
         ;;
     i686-unknown-linux-gnu)
+        export PKG_CONFIG="i686-linux-gnu-pkg-config"
         export PKG_CONFIG_ALLOW_CROSS=1
         cargo build --verbose --all --target="$TARGET"
         ;;
